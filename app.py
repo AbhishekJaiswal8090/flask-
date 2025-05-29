2f7db47cdba3871caceb812153ddd2ce3ee32016
Abhishek <abhi84514aaa.in@gmail.com>
2025-05-30 00:12:27 +0530

    Basic setup of the flask framework in python

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+'''
+It creates an instance of the flask
+which will be your wsgi (web server gateway interface) application
+
+
+'''
+#BASIC SETUP
+
+#WSGI APPlication
+app=Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "welcome to this flash course Hello brother This should be amazing course"
+
+@app.route("/web")
+def web():
+    return "Welcome to th web page"
+
+if __name__=="__main__":
+    app.run(debug=True)
