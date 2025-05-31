cc0de2b2f8f01c791a90d3537e71abf810f048b1
Abhishek <abhi84514aaa.in@gmail.com>
2025-05-31 05:45:16 +0530

    learn the concept of redirecting in python

--- jinja.py
+++ jinja.py
@@ -8,7 +8,7 @@
 {#..#} this is for comments
 
 '''
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app =Flask(__name__)
 
@@ -59,7 +59,26 @@ def successres(score):
 def successif(score):
     res=''
    
-    return render_template('result2.html',results=score)
+    return render_template('getres.html',results=score)
 
+
+@app.route('/fail/<int:score>') 
+def fail(score):
+   
+    return render_template('result2.html',results=score)
+@app.route('/submit', methods=['POST','GET'])
+def submit():
+    total_score = 0
+    if request.method == 'POST':
+        science = float(request.form['science'])
+        maths = float(request.form['maths'])
+        c = float(request.form['c'])
+        data_science = float(request.form['datascience'])
+
+        total_score = (science + maths + data_science + c) / 4
+    else:
+        return render_template('getres.html')
+    return redirect(url_for('successres', score=total_score))
+    
 if __name__=="__main__":
     app.run(debug=True)
